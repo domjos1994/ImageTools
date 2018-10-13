83ffba7eef65ebc52843678cfa8fc87269fd9bd2
domjos1994 <developing@domjos.de>
2018-10-13 13:11:21 +0200

    update process-task

---
+++ Classes/Task/CompressTask.php
@@ -0,0 +1,59 @@
+<?php
+namespace DominicJoas\Imgcompromizer\Task;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
+
+class CompressTask extends \TYPO3\CMS\Scheduler\Task\AbstractTask {
+    private $objectManager;
+
+
+    public function execute() {
+        try {
+            $this->scheduler->log("Anfang", 0, 0);
+            $this->objectManager = GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\Object\ObjectManager::class);
+            $this->initTinify();
+
+            $fileRepository = $this->objectManager->get(\DominicJoas\Imgcompromizer\Domain\Repository\FileRepository::class);
+            $persistenceManager = $this->objectManager->get("TYPO3\\CMS\\Extbase\\Persistence\\Generic\\PersistenceManager");
+            foreach($fileRepository->getAllEntries() as $file) {
+                $this->updateFile($file, $persistenceManager);
+            }
+            
+            
+            $persistenceManager->persistAll();
+        } catch (Exception $ex) {
+            $this->scheduler->log("Fehler", 0, 0);
+            return false;
+        }
+        
+        return true;
+    }
+    
+    private function initTinify() {
+        $configurationManager = GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\Configuration\ConfigurationManager::class);
+        $tinifyKey = $configurationManager->getConfiguration(ConfigurationManagerInterface::CONFIGURATION_TYPE_FRAMEWORK, "imgcompromizer_module1")['settings']['tinifyKey'];
+        
+        $extManUtility = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::class);
+        $extPath = $extManUtility::extPath("imgcompromizer");
+        require_once($extPath . 'Resources/Private/PHP/lib/Tinify/Exception.php');
+        require_once($extPath . 'Resources/Private/PHP/lib/Tinify/ResultMeta.php');
+        require_once($extPath . 'Resources/Private/PHP/lib/Tinify/Result.php');
+        require_once($extPath . 'Resources/Private/PHP/lib/Tinify/Source.php');
+        require_once($extPath . 'Resources/Private/PHP/lib/Tinify/Client.php');
+        require_once($extPath . 'Resources/Private/PHP/lib/Tinify.php');
+        \Tinify\setKey($tinifyKey);
+    }
+    
+    private function updateFile($file, $persistenceManager) {
+        $absoluteFile = $file->getOriginalResource()->getContents();
+
+        $source = \Tinify\fromBuffer($absoluteFile);
+        
+        $file->getOriginalResource()->setContents($source->toBuffer());
+        $file->setTxImgcompromizerCompressed(1);
+        $this->scheduler->log("Speichern", 0, 0);
+        $persistenceManager->update($file);
+    }
+}
+
--- ext_localconf.php
+++ ext_localconf.php
@@ -6,4 +6,11 @@
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Extbase\\Domain\\Model\\File'] = array(
         'className' => 'DominicJoas\Imgcompromizer\Domain\Model\File'
     );
+    
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\DominicJoas\Imgcompromizer\Task\CompressTask::class] = [
+        'extension' => 'imgcompromizer',
+        'title' => 'Compress images',
+        'description' => 'Compress all images which aren\'t compressed!',
+        'additionalFields' => '',
+    ];
 
